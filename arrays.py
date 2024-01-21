c3f20d9e395c67492bdb7dbb8a45331a4fcc6d1e
Daniel Agg <daniel.agg@outlook.com>
2024-01-21 18:40:29 +0100

    solve isAnagram

--- arrays.py
+++ arrays.py
@@ -5,3 +5,9 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashSet = {i for i in nums}
         return len(hashSet) != len(nums)
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+
+print(Solution().isAnagram("test", "tsset"))
